4cfbc522515820abbb186109f72d71d35ae8d737
jelopes1 <jlopes192@gmail.com>
2022-06-08 18:12:16 +0200

    ajout des fichiers au dépôt

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+    class HomeController extends AbstractController{
+
+ 
+ /** 
+ *@Route("/", name="home")
+ */
+        public function index ()
+        {
+                return $this ->render("home/index.html.twig",[
+                     "title" => "Bienvenue sur mon site",
+                     "message" => "Hello world ! Bienvenue sur mon site",
+                     "menu" => "home"
+
+                ]);
+                
+        }
+
+
+        /** 
+ *@Route("/contact", name="contact")
+ */
+
+        public function contact()
+        {
+        return $this ->render("home/contact.html.twig",[
+
+              "menu" =>"contact"
+        ]);
+        }
+
+    }
