20cc27e0ee5e8cf0bc21e3e524a9acfd6ce75bb4
jelopes1 <jlopes192@gmail.com>
2022-06-09 17:22:15 +0200

    envoi

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Gite;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
     class HomeController extends AbstractController{
 
  
  /** 
  *@Route("/", name="home")
  */
-        public function index ()
+        public function index (ManagerRegistry $doctrine)
         {
+                     $repository = $doctrine ->getRepository(Gite::class);
+                     $gites = $repository->findAll();
+dump($gites);
+
+             /* $manager = $doctrine ->getManager();
+
+              $gite = new Gite();
+              $gite -> setNom("Mon premier Gite")
+                     -> setDescriptiion("Et consequat ut velit velit qui magna nulla dolore id irure ullamco non ullamco.")
+                     -> setSurface(400)
+                     -> setCouchage(5)
+                     ->setChambres(3);
+
+
+              $manager ->persist($gite);
+              $manager->flush(); */
+
                 return $this ->render("home/index.html.twig",[
                      "title" => "Bienvenue sur mon site",
                      "message" => "Hello world ! Bienvenue sur mon site",
-                     "menu" => "home"
+                     "menu" => "home",
+                     "gites" => $gites
 
                 ]);
                 
