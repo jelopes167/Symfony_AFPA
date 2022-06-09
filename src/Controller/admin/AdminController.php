dfedf1af23094ce841744474cf8c01ffae4451b6
jelopes1 <jlopes192@gmail.com>
2022-06-09 19:22:02 +0200

    envoi

---
+++ src/Controller/admin/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class AdminController extends AbstractController
+{
+
+    /**
+     * @Route ("/admin", name="admin_index")
+     */
+    public function index() {
+        return $this ->render ("admin/index.html.twig");
+    }
+}
